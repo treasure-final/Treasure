c1c166530642a050b19d2f3b57e90eb6a92ca3b9
Ju Harang <126046474+proud98@users.noreply.github.com>
2023-05-03 10:28:15 +0900

    day0503 회원가입

--- src/main/java/boot/mvc/user/UserServiceInter.java
+++ src/main/java/boot/mvc/user/UserServiceInter.java
@@ -4,5 +4,14 @@ import java.util.Map;
 
 public interface UserServiceInter {
     public int checkIdAndPassword(String email, String pass);
+    
+    //가입
+  	public void insertJoinUser(UserDto dto);
+  	
+  	//이메일 중복 체크
+  	public int userSearchEmail(String user_email);
+  	
+  	//닉네임 중복 체크
+  	public int userSearchNickname(String user_nickname);
 
 }

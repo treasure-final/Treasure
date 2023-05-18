a5fe9e4f90e3168863f6ea7c8cd6e51fcdcd2abb
Ju Harang <126046474+proud98@users.noreply.github.com>
2023-05-18 11:09:09 +0900

    DAY0518 MERGE 후

--- src/main/java/boot/mvc/detail/DetailController.java
+++ src/main/java/boot/mvc/detail/DetailController.java
@@ -16,41 +16,35 @@ import boot.mvc.item.ItemDto;
 @Controller
 public class DetailController {
 
-	@Autowired
-	DetailService Dservice;
+   @Autowired
+   DetailService Dservice;
 
-	@GetMapping("/item/detail")
-	public String detail(String item_num,Model model) {
-		model.addAttribute("item_num",item_num);
-		return "/3/item/detail";
-	}
+   @GetMapping("/item/detail")
+   public ModelAndView detail(String item_num, String buy_size, Model model) {
+      ModelAndView mview = new ModelAndView();
 
-	@GetMapping("/item/DetailgetData")
-	public ModelAndView DetailgetData(String Item_num) {
-		ModelAndView mview = new ModelAndView();
+      List<ItemDto> list = Dservice.getAllData();
+      ItemDto dto = Dservice.DetailgetData(item_num);
+      
+      mview.addObject("list", list);
+      mview.addObject("Ddto", dto);
 
-		List<ItemDto> list = Dservice.getAllData();
-		ItemDto dto = Dservice.DetailgetData(item_num);
+      mview.setViewName("/3/item/detail");
 
-		mview.addObject("list", list);
-		mview.addObject("Ddto", dto);
+      model.addAttribute("item_num", item_num);
+      return mview;
+   }
 
-		mview.setViewName("/3/item/detail");
-
-		model.addAttribute("item_num", item_num);
-		return mview;
-	}
-
-//	@GetMapping("/item/detail")
-//	public ModelAndView DetailgetData(@RequestParam String item_num) {
-//		ModelAndView mview = new ModelAndView();
+//   @GetMapping("/item/detail")
+//   public ModelAndView DetailgetData(@RequestParam String item_num) {
+//      ModelAndView mview = new ModelAndView();
 //
-//		ItemDto dto = Dservice.DetailgetData(item_num);
+//      ItemDto dto = Dservice.DetailgetData(item_num);
 //
-//		mview.addObject("Ddto", dto);
+//      mview.addObject("Ddto", dto);
 //
-//		mview.setViewName("/item/detail");
+//      mview.setViewName("/item/detail");
 //
-//		return mview;
-//	}
+//      return mview;
+//   }
 }

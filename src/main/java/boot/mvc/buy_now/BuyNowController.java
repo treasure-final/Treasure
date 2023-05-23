219995b8a9ffbdd1b5689e31b3364fbcbee529c5
ez0 <101056943+easyoungcode@users.noreply.github.com>
2023-05-23 10:16:41 +0900

    즉시구매, order 테이블에 추가, buyNow sql문 수정

--- src/main/java/boot/mvc/buy_now/BuyNowController.java
+++ src/main/java/boot/mvc/buy_now/BuyNowController.java
@@ -2,6 +2,7 @@ package boot.mvc.buy_now;
 
 import boot.mvc.item.ItemDto;
 import boot.mvc.item.ItemService;
+import boot.mvc.order.OrderDto;
 import boot.mvc.order.OrderService;
 import boot.mvc.sell_bid.SellBidDto;
 import boot.mvc.sell_bid.SellBidService;
@@ -13,7 +14,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -118,6 +122,7 @@ public class BuyNowController {
         String loginEmail = (String) session.getAttribute("loginEmail");
         String userNum = userService.findEmailUserNum(loginEmail);
         UserDto userDto = userService.getUserNumData(userNum);
+
         mv.addObject("userName", userDto.getUser_name());
         mv.addObject("userPhone", userDto.getUser_hp());
         mv.addObject("size", size);
@@ -127,14 +132,61 @@ public class BuyNowController {
     }
 
     @PostMapping("/buy/orderproc")
+    @ResponseBody
     public String orderproc(HttpSession session,
-                            String item_num, String wish_price, String price,
-                            String size) {
+                            String item_num, String wish_price,
+                            String size, String delivery,
+                            String buy_addr,
+                            String payment) {
+        String loginEmail=(String)session.getAttribute("loginEmail");
+        String user_num=userService.findEmailUserNum(loginEmail);
+        List<SellBidDto> list=service.getBuyNowPrice(item_num);
+        String sell_user=list.get(0).getUser_num();
+        String sell_num=list.get(0).getSell_num();
+
         System.out.println(item_num);
         System.out.println(wish_price);
         System.out.println(size);
-        System.out.println(price);
+        System.out.println(sell_user);
+        System.out.println(sell_num);
+        System.out.println(delivery);
+        System.out.println(buy_addr);
+        System.out.println(payment);
 
-        return "redirect:ordersuccess?item_num="+item_num+"&size="+size;
+        //order에 insert
+//        ItemDto itemDto=itemService.getItemData(item_num);
+        OrderDto orderDto=new OrderDto();
+        orderDto.setSell_user(sell_user);
+        orderDto.setItem_num(item_num);
+        orderDto.setBuy_user(user_num);
+        orderDto.setSize(size);
+        orderDto.setWish_price(Integer.parseInt(wish_price));
+        orderService.insertOrder(orderDto);
+
+        //buy_now에 insert
+        BuyNowDto buyNowDto=new BuyNowDto();
+        buyNowDto.setUser_num(user_num);
+        buyNowDto.setItem_num(item_num);
+        System.out.println("아이템:"+buyNowDto.getItem_num());
+        buyNowDto.setSell_num(sell_num);
+        buyNowDto.setBuynow_price(Integer.parseInt(wish_price));
+        if(delivery.equals("fast")){
+            buyNowDto.setDelivery("빠른배송");
+        }else {
+            buyNowDto.setDelivery("일반배송");
+        }
+        buyNowDto.setBuy_addr(buy_addr);
+        if(payment.equals("kakaopay")) {
+            buyNowDto.setPayment("카카오페이");
+        } else {
+            buyNowDto.setPayment("신용카드");
+        }
+        buyNowDto.setBuy_status(Math.random()<0.5?0:1);
+        service.insertBuyNow(buyNowDto);
+
+        //sell_bid에서 판매상태 '판매완료'로 변경
+        service.updateSellStatus(sell_num);
+
+        return item_num;
     }
 }

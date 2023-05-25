84a6ceae98319cb8e74e7a08744e9fc9b4317b1f
ez0 <101056943+easyoungcode@users.noreply.github.com>
2023-05-25 18:44:42 +0900

    boardLike 게시판 좋아요 기능 추가

--- src/main/java/boot/mvc/board_like/BoardLikeMapperInter.java
+++ src/main/java/boot/mvc/board_like/BoardLikeMapperInter.java
@@ -2,9 +2,14 @@ package boot.mvc.board_like;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface BoardLikeMapperInter {
 
-	
-	public void findLike(int board_num,int user_num);
+	public void insertBoardLike(BoardLikeDto boardLikeDto);
+	public void deleteBoardLike(Map<String,String> map);
+	public List<BoardLikeDto> getUsersBoardLike(String user_num);
+	public int boardLikeCheck(Map<String,String> map);
 }
--- src/main/java/boot/mvc/board_like/BoardLikeServiceInter.java
+++ src/main/java/boot/mvc/board_like/BoardLikeServiceInter.java
@@ -1,5 +1,11 @@
 package boot.mvc.board_like;
 
+import java.util.List;
+import java.util.Map;
+
 public interface BoardLikeServiceInter {
-	
+    public void insertBoardLike(BoardLikeDto boardLikeDto);
+    public void deleteBoardLike(String board_id, String user_num);
+    public List<BoardLikeDto> getUsersBoardLike(String user_num);
+    public int boardLikeCheck(String board_id, String user_num);
 }
